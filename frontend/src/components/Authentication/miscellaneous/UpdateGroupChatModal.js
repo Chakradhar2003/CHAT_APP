fd979a04d29a964e022a8651bb7117af9095915b
DEV-ansh7 <118380070+DEV-ansh7@users.noreply.github.com>
2023-07-03 22:17:38 +0530

    Update UpdateGroupChatModal.js

--- frontend/src/components/Authentication/miscellaneous/UpdateGroupChatModal.js
+++ frontend/src/components/Authentication/miscellaneous/UpdateGroupChatModal.js
@@ -22,7 +22,7 @@ import {
   import UserBadgeItem from '../../UserAvatar/UserBadgeItem';
   import UserListItem from '../../UserAvatar/UserListItem';
 
-const UpdateGroupChatModal = ({fetchAgain, setFetchAgain}) => {
+const UpdateGroupChatModal = ({fetchAgain, setFetchAgain,fetchMessages}) => {
     const { isOpen, onOpen, onClose } = useDisclosure();
     const [groupChatName, setGroupChatName] = useState();
     const [search, setSearch] = useState("");
@@ -64,6 +64,7 @@ const UpdateGroupChatModal = ({fetchAgain, setFetchAgain}) => {
 
       user1._id === user._id ? setSelectedChat() : setSelectedChat(data);
       setFetchAgain(!fetchAgain);
+        fetchMessages();
       setLoading(false);
     } catch (error) {
       toast({
