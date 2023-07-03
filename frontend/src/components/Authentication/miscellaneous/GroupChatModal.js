fd6894758f71a03561ce26b12664bc6345b839db
DEV-ansh7 <118380070+DEV-ansh7@users.noreply.github.com>
2023-07-03 20:41:03 +0530

    Update GroupChatModal.js

--- frontend/src/components/Authentication/miscellaneous/GroupChatModal.js
+++ frontend/src/components/Authentication/miscellaneous/GroupChatModal.js
@@ -51,7 +51,6 @@ const GroupChatModal = ({children}) => {
         },
       };
       const { data } = await axios.get(`/api/user?search=${search}`, config);
-      console.log(data);
       setLoading(false);
       setSearchResult(data);
     } catch (error) {
