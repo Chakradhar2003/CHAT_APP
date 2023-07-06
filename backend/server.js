31896cc7ecb2de16741bfb8e1e38289baf478a45
lalhriemsang <138213648+lalhriemsang@users.noreply.github.com>
2023-07-06 14:55:39 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use('/api/message', messageRoutes);
 
 const __dirname1 = path.resolve();
 
-if(process.env.NODE_ENV=='production'){
+if(process.env.NODE_ENV ==="production"){
 
     app.use(express.static(path.join(__dirname1,"/frontend/build")));
 
