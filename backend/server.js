c6d2e05a3b3d5d70998b6284503a8307f1ea8631
lalhriemsang <138213648+lalhriemsang@users.noreply.github.com>
2023-07-06 14:41:11 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ const app = express();
 
 var cors = require('cors');
 const { notFound, errorHandler } = require('./middleware/errorMiddleware')
+const path = require("path");
 
 app.use(cors());
 
@@ -23,6 +24,26 @@ app.get('/', (req, res) => {
 app.use('/api/user', userRoutes);
 app.use('/api/chat', chatRoutes);
 app.use('/api/message', messageRoutes);
+
+//---Deployment----
+
+const __dirname1 = path.resolve();
+
+if(process.env.NODE_ENV=='production'){
+
+    app.use(express.static(path.join(__dirname1,"/frontend/build")));
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname1,"frontend","build","index.html"));
+    });
+    
+} else {
+    app.get("/",(req,res) => {
+        res.send("API is Running Successfully");
+    });
+}
+
+//---Deployment----
 // app.get('/api/chat', (req, res) => {
 //     res.send(chats);
 // })
