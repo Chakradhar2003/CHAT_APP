29d538ea8bdc5509df1a6247a76c05c0e327ff24
Chakradhar2003 <95245987+Chakradhar2003@users.noreply.github.com>
2023-07-06 22:32:33 +0530

    Update server.js
    Deployment cancelled

--- backend/server.js
+++ backend/server.js
@@ -26,19 +26,19 @@ app.use('/api/message', messageRoutes);
 
 const __dirname1 = path.resolve();
 
-if(process.env.NODE_ENV ==="production"){
+// if(process.env.NODE_ENV ==="production"){
 
-    app.use(express.static(path.join(__dirname1,"/frontend/build")));
+//     app.use(express.static(path.join(__dirname1,"/frontend/build")));
 
-    app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname1,"frontend","build","index.html"));
-    });
+//     app.get("*",(req,res)=>{
+//         res.sendFile(path.resolve(__dirname1,"frontend","build","index.html"));
+//     });
     
-} else {
-    app.get('/', (req, res) => {
-        res.send("API is running")
-    });
-}
+// } else {
+//     app.get('/', (req, res) => {
+//         res.send("API is running")
+//     });
+// }
 
 //---Deployment----
 // app.get('/api/chat', (req, res) => {
